8641f5ff3f628d8a5fe1bb06588efd07878dc657
ktshen <bruce20036@gmail.com>
2018-05-30 23:04:27 +0800

    Update Docker

--- compose/flask/gunicorn.sh
+++ compose/flask/gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/bin/gunicorn -w 2 -b 0.0.0.0:5000 --chdir=/app --worker-class=gevent wsgi:application
+/usr/local/bin/gunicorn -w 2 -b 0.0.0.0:5000 --worker-class=eventlet --chdir=/app wsgi:application
